3d44de00013fd67ab45c45fd5e04110e494f471f
shies01 <shk.sakib.official@gmail.com>
2024-02-04 00:14:19 +0530

    added Day-1 javascript assignments

---
+++ Day-1/Introduction to JS - Variables & Data Types/prob4.js
@@ -0,0 +1,8 @@
+let name = "John";
+let school = "Masai School";
+let grade = "A";
+let section = "B";
+let rollno = "123";
+let marks = "Maths - 90, Science - 80, English - 70";
+let reportcard = "Name :" + name + "\n" + "School :" + school + "\n" + "Grade :" + grade + "\n" + "Section :" + section + "\n" + "Roll No :" + rollno + "\n" + "Marks :" + marks;
+console.log(reportcard);
